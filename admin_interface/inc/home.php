5d369d3a8caf6f920957ca8e12feb2bac1e88ade
ezitounioussama <zitounioussama95@gmail.com>
2022-06-15 17:40:37 +0100

    update dont work

--- admin_interface/inc/home.php
+++ admin_interface/inc/home.php
@@ -1,37 +1,124 @@
 <?php require_once 'header.php';
 require_once 'nav.php';
 ?>
+<?php
+// Include config file
+require_once "config.php";
 
+// Define variables and initialize with empty values
+$name = $CIN = $pass = $function  =  "";
+$name_err = $CIN_err =  $pass_err = $function_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if (empty($input_name)) {
+        $name_err = "Please enter a name.";
+    } else {
+        $name = $input_name;
+    }
+
+    // Validate Serie
+    $input_CIN = trim($_POST["cin"]);
+    if (empty($input_CIN)) {
+        $CIN_err = "Please enter your CIN";
+    } else {
+        $CIN = $input_CIN;
+    }
+
+
+    // Validate Serie
+    $input_pass = trim($_POST["pass"]);
+    if (empty($input_pass)) {
+        $pass_err = "Please enter a password";
+    } else {
+        $pass = $input_pass;
+    }
+
+    // Validate status
+    $input_function = trim($_POST["function"]);
+    if (empty($input_function)) {
+        $function_err = "Please enter a function";
+    } else {
+        $function = $input_function;
+    }
+
+
+
+
+
+
+
+
+    // Check input errors before inserting in database
+    if (empty($name_err) && empty($CIN_err) && empty($pass_err) && empty($function_err)) {
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, CIN , pass, function) VALUES (:username, :CIN, :pass, :function)";
+
+        if ($stmt = $pdo->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_name);
+            $stmt->bindParam(":CIN", $param_CIN);
+            $stmt->bindParam(":pass", $param_pass);
+            $stmt->bindParam(":function", $param_function);
+            // Set parameters
+            $param_name = $name;
+            $param_CIN = $CIN;
+            $param_pass = $pass;
+            $param_function = $function;
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // Records created successfully. Redirect to landing page
+                header("location: home.php");
+                exit();
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+    }
+
+    // Close connection
+    unset($pdo);
+}
+?>
 
 
 <div class="container mx-auto">
     <div class="max-w-xl p-5 mx-auto my-10 bg-white rounded-md shadow-sm">
         <div class="text-center">
-            <h1 class="my-3 text-3xl font-semibold text-gray-700">Contact Us</h1>
-            <p class="text-gray-400">Fill up the form below to send us a message.</p>
+            <h1 class="my-3 text-3xl font-semibold text-gray-700">Insert Users</h1>
+
         </div>
         <div>
-            <form action="" method="POST">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                 <div class="mb-6">
-                    <label for="name" class="block mb-2 text-sm text-gray-600">Full Name</label>
+                    <label for="name" class="block mb-2 text-sm text-gray-600">Username</label>
                     <input type="text" name="name" placeholder="John Doe" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
                 </div>
                 <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm text-gray-600">Email Address</label>
-                    <input type="email" name="email" placeholder="you@email.com" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
+                    <label for="email" class="block mb-2 text-sm text-gray-600">CIN</label>
+                    <input type="text" name="cin" placeholder="XX-----" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
                 </div>
                 <div class="mb-6">
-                    <label for="phone" class="text-sm text-gray-600">Phone Number</label>
-                    <input type="text" name="phone" placeholder="91 1234-567" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
+                    <label for="phone" class="text-sm text-gray-600">Password</label>
+                    <input type="password" name="pass" placeholder="*********" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
                 </div>
-                <div class="mb-6">
-                    <label for="message" class="block mb-2 text-sm text-gray-600">Your Message</label>
+                <div class="col-span-6 sm:col-span-3">
+                    <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+                    <select id="role" name="function" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <option>user</option>
+                        <option>admin</option>
 
-                    <textarea rows="5" name="message" placeholder="Your Message" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" required></textarea>
+                    </select>
                 </div>
-                <div class="mb-6">
-                    <button type="submit" class="w-full px-2 py-4 text-white bg-indigo-500 rounded-md  focus:bg-indigo-600 focus:outline-none">
-                        Send Message
+                <div class="mt-10 col-span-6">
+                    <button class="rounded-lg bg-black text-sm p-2.5 text-white w-full block" type="submit" name="submit">
+                        Insert
                     </button>
                 </div>
             </form>
@@ -62,6 +149,7 @@ require_once 'nav.php';
             echo '<th class="px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap">CIN</th>';
             echo '<th class="px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap">Password</th>';
             echo '<th class="px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap">Role</th>';
+            echo '<th class="px-4 py-2 font-medium text-left text-gray-900 whitespace-nowrap" colspan="2"></th>';
             echo '</tr>';
             echo '</thead>';
             echo '<tbody class="divide-y divide-gray-100">';
@@ -72,6 +160,24 @@ require_once 'nav.php';
                 echo '<td class="px-4 py-2 text-gray-700 whitespace-nowrap">' . $row["CIN"] . '</td>';
                 echo '<td class="px-4 py-2 text-gray-700 whitespace-nowrap">' . $row["pass"] . '</td>';
                 echo '<td class="px-4 py-2 text-gray-700 whitespace-nowrap">' . $row["function"] . '</td>';
+                echo '<td class="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                        <a href="update.php?id=' . $row['UserId'] . '" class="modal-open text-indigo-600 hover:text-indigo-900" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </a>
+    
+                      </td>';
+                echo  '<td class="text-sm font-medium leading-5 whitespace-no-wrap border-b border-gray-200 ">
+                  <a href="delete.php?id=' . $row['UserId'] . '"><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-600 hover:text-red-800"
+                      fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    </svg></a>
+  
+                      </td>';
                 echo '</tr>';
             }
             echo '</tbody>';
---
+++ admin_interface/inc/update.php
@@ -0,0 +1,201 @@
+<?php
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$name = $CIN = $pass = $function  =  "";
+$name_err = $CIN_err =  $pass_err = $function_err = "";
+
+// Processing form data when form is submitted
+if (isset($_POST["UserId"]) && !empty($_POST["UserId"])) {
+    // Get hidden input value
+    $id = $_POST["UserId"];
+
+    $input_name = trim($_POST["name"]);
+    if (empty($input_name)) {
+        $name_err = "Please enter a name.";
+    } else {
+        $name = $input_name;
+    }
+
+    // Validate Serie
+    $input_CIN = trim($_POST["cin"]);
+    if (empty($input_CIN)) {
+        $CIN_err = "Please enter your CIN";
+    } else {
+        $CIN = $input_CIN;
+    }
+
+
+    // Validate Serie
+    $input_pass = trim($_POST["pass"]);
+    if (empty($input_pass)) {
+        $pass_err = "Please enter a password";
+    } else {
+        $pass = $input_pass;
+    }
+
+    // Validate status
+    $input_function = trim($_POST["function"]);
+    if (empty($input_function)) {
+        $function_err = "Please enter a function";
+    } else {
+        $function = $input_function;
+    }
+
+    // Check input errors before inserting in database
+    if (empty($name_err) && empty($CIN_err) && empty($pass_err) && empty($function_err)) {
+        // Prepare an update statement
+        $sql = "UPDATE users SET username=:username, CIN=:CIN, pass=:pass, function=:function WHERE UserId=:UserId";
+
+        if ($stmt = $pdo->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_name);
+            $stmt->bindParam(":CIN", $param_CIN);
+            $stmt->bindParam(":pass", $param_pass);
+            $stmt->bindParam(":function", $param_function);
+            $stmt->bindParam(":UserId", $param_id);
+
+            // Set parameters
+            $param_name = $name;
+            $param_CIN = $CIN;
+            $param_pass = $pass;
+            $param_function = $function;
+            $param_id = $id;
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // Records updated successfully. Redirect to landing page
+                header("location: home.php");
+                exit();
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+    }
+
+    // Close connection
+    unset($pdo);
+} else {
+    // Check existence of id parameter before processing further
+    if (isset($_GET["UserId"]) && !empty(trim($_GET["UserId"]))) {
+        // Get URL parameter
+        $id =  trim($_GET["UserId"]);
+
+        // Prepare a select statement
+        $sql = "SELECT * FROM users WHERE UserId = :UserId";
+        if ($stmt = $pdo->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":UserId", $param_id);
+
+            // Set parameters
+            $param_id = $id;
+
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                if ($stmt->rowCount() == 1) {
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    // Retrieve individual field value
+                    $name = $row["username"];
+                    $CIN = $row["CIN"];
+                    $pass = $row["pass"];
+                    $function = $row["function"];
+                } else {
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+
+        // Close connection
+        unset($pdo);
+    } else {
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+
+<?php
+require_once 'header.php';
+?>
+
+
+
+
+
+<section>
+
+
+
+
+    <div class="container mx-auto">
+        <div class="max-w-xl p-5 mx-auto my-10 bg-white rounded-md shadow-sm">
+            <div class="text-center">
+                <h1 class="my-3 text-3xl font-semibold text-gray-700">Insert Users</h1>
+
+            </div>
+            <div>
+                <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="POST">
+                    <div class="mb-6">
+                        <label for="name" class="block mb-2 text-sm text-gray-600">Username</label>
+                        <input type="text" name="name" placeholder="John Doe" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" value="<?php echo $name; ?>" />
+                    </div>
+                    <div class="mb-6">
+                        <label for="email" class="block mb-2 text-sm text-gray-600">CIN</label>
+                        <input type="text" name="cin" placeholder="XX-----" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" value="<?php echo $CIN; ?>" />
+                    </div>
+                    <div class="mb-6">
+                        <label for="phone" class="text-sm text-gray-600">Password</label>
+                        <input type="password" name="pass" placeholder="*********" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md  focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" value="<?php echo $pass; ?>" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-3">
+                        <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+                        <select id="role" name="function" autocomplete="country" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" value="<?php echo $function; ?>">
+                            <option>user</option>
+                            <option>admin</option>
+
+                        </select>
+                    </div>
+                    <div class="col-span-6">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                        <button class="rounded-lg bg-black text-sm p-2.5 text-white w-full block" type="submit" name="submit">
+                            Insert
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require_once 'footer.php';
+?>
