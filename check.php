11eddf2fc523df7c125f7dc03e2d93060cbfa5e6
ezitounioussama <zitounioussama95@gmail.com>
2022-06-26 19:01:11 +0100

    everything's works great

--- check.php
+++ check.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $passe = $_POST['pass'];
     $cin = $_POST['cin'];
     // $dbh = new PDO('mysql:host=localhost;dbname=webapp', $user = "root", $pass = "");
-    include '../db/db.php';
+    include 'db/db.php';
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $conn->prepare('SELECT * FROM users WHERE  CIN=? and username=? and pass=?');
     $parametres = array($cin, $log, $passe);
@@ -18,9 +18,9 @@ if (isset($_POST['submit'])) {
         $_SESSION['username'] = $log;
         $_SESSION['info'] = $cin;
         if ($rs['function'] == "admin") {
-            header('location:../admin_interface/inc/home.php');
+            header('location:admin_interface/inc/home.php');
         } elseif ($rs['function'] == "user") {
-            header('location:../user_interface/inc/home.php');
+            header('location:user_interface/inc/home.php');
         }
     } else {
         header('location:../error.php');
